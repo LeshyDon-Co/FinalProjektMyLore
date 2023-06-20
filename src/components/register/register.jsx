0b73f699ffc556c4c23644c3866e7000db417dc7
Madeline <madeline.streubel@gmx.de>
2023-06-20 10:54:21 +0200

    DatenbankConnection hergestellt. Der User wird in die Datenbank geschrieben. Noch keine Validierung!

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+"use client";
+import Link from 'next/link';
+// import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
 import styles from "./register.module.css";
 
-function Register() {
+//--------------------------------------------------------------------//
+const Register = () => {
+
+  //const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+//--------------------------------------------------------------------//
+  const registerUser = async (e) => {
+    e.preventDefault();
+    console.log("registriere User");
+
+    //Senden der eingegebenen Informationen an den api/register-Endpunkt.
+    //Dieser sendet eine Antwort, die wie in "res" speichern und dann abfragen.
+    //Wenn der Status dieser Antwort 201 ist, bedeutet das, die Registrierung war erfolgreich
+    try {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          email,
+          password,
+        }),
+      });
+
+      if(res.status === 201){
+        console.log("User registriert!!!");
+      }
+    } catch (error) {
+        setError(true);
+    }
+
+    setUsername("");
+    setEmail("");
+    setPassword("");
+
+  }
+
+//--------------------------------------------------------------------//
   return (
     <div>
         <h2>Registrierung</h2>
@@ -9,32 +55,36 @@ function Register() {
           <div>
             <label>Username</label>
             <input
-              // value={username}
+              value={username}
               type='text'
               required
               placeholder='Username'
+              onChange={(e) => {setUsername(e.target.value)}}
             />
           </div>
           <div>
             <label>Email-Adresse</label>
             <input
-              // value={email}
+              value={email}
               type='email'
               required
               placeholder='Email'
+              onChange={(e) => {setEmail(e.target.value)}}
             />
           </div>
           <div>
             <label>Passwort</label>
             <input 
-              // value={password}
+              value={password}
               type='password'
               required
               placeholder='Password'
+              onChange={(e) => {setPassword(e.target.value)}}
             />
           </div>
-          <button>Registrieren</button>
+          <button onClick={registerUser}>Registrieren</button>
         </form>
+        {error && "Etwas ist schief gelaufen!"};
     </div>
   )
 }

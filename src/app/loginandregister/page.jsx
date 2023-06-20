35d00e2f24112d833ab9bb156e51633a47760dc1
Don-Strevo <steven.guenther@dci.education>
2023-06-20 20:46:31 +0200

    Überarbeitung Design LoginPage + Platzhalter für Validierungsnachrichten eingerichtet

--- src/app/loginandregister/page.jsx
+++ src/app/loginandregister/page.jsx
@@ -1,14 +1,34 @@
 import Register from "@/components/register/register";
 import Login from "@/components/login/login";
 import React from "react";
+import Link from "next/link";
 import styles from "./page.module.css";
 
 function LoginandRegister() {
   return (
-    <div>
-      <h1>Login und Register</h1>
-      <Register />
-      <Login />
+    <div className={styles.body}>
+      {/* <h2>Registrierung und Login</h2> */}
+      <div className={styles.loginregistercontainer}>
+        <div className={styles.registercontainer}>
+          <Register />
+        </div>
+        <div className={styles.p}>
+          <p>
+            Hier kommen die Statusmeldungen über Registrierung und Validierung
+            rein.
+          </p>
+          {/* {showRegistrysuccessfull && (
+            <p className="RegisterMailInfo">Mail ist unterwegs!</p>
+          )}
+          {myRegisterError && (
+            <p className="myRegisterError">{myRegisterError}</p>
+          )} */}
+        </div>
+        <div className={styles.logincontainer}>
+          <Login />
+        </div>
+      </div>
+      {/* <Link href="/">zurück zur Startseite</Link> */}
     </div>
   );
 }

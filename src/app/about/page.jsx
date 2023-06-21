0dd449e6a1488713ced0f4281e2c09993d340cfc
Siick-CattoO <c.j.maslowski@outlook.de>
2023-06-21 11:53:50 +0200

    wraped container-div in body-div & set body css for every page.module.css

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
 import styles from "./page.module.css";
 
 function About() {
   return (
-    <div className={styles.container}>Hier gibt es Informationen über das Projekt und das Team, welches dahinter steht</div>
-  )
+    <div className={styles.body}>
+      <div className={styles.container}>
+        Hier gibt es Informationen über das Projekt und das Team, welches dahinter
+        steht
+      </div>
+    </div>
+  );
 }
 
 export default About;

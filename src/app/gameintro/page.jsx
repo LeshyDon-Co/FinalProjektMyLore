63da090af4f7ac6026334fcb3f15402253e8c6a3
Don-Strevo <steven.guenther@dci.education>
2023-06-29 15:57:52 +0200

    introtext and introimage div created

--- src/app/gameintro/page.jsx
+++ src/app/gameintro/page.jsx
@@ -1,17 +1,31 @@
 import styles from "./page.module.css";
 import Story from "@/assets/storytelling/story";
 import Buttonone from "@/components/buttons/buttonnormal/buttonnormal";
+import Introimage from "@/components/explanation/introimage/introimage";
 import Introtext from "@/components/explanation/introtext/introtext";
 import React from "react";
+import * as story from "../../assets/storytelling/maindata.json";
+
+// const mainStory = {textMainStory};
 
 function GameIntro() {
   return (
     <div className={styles.body}>
-      <Story />
-      <div>
-        <div className={styles.introtext}></div>
-        <Introtext text={"moinmoin"} />
-        <Buttonone text={"Rette die Welt ->"} link={"/game"} />
+      {/* <Story /> */}
+      <div className={styles.h2}>
+        <h2>Game-Intro</h2>
+      </div>
+      <div className={styles.intro}>
+        <Introimage pic={"Image"} />
+        <Introtext text={"Text"} />
+      </div>
+      <div className={styles.footer}>
+        <div></div>
+        <Buttonone
+          text={"Beginne mit der Rettung der Welt ->"}
+          link={"/game"}
+          className={styles.continue}
+        />
       </div>
     </div>
   );

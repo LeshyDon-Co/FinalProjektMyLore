8d30183cf656663c830a043e17f77bc389c7f16a
Don-Strevo <steven.guenther@dci.education>
2023-06-29 11:19:24 +0200

    save character in databank with validation

--- src/app/api/auth/character/route.js
+++ src/app/api/auth/character/route.js
@@ -1,57 +1,59 @@
 import Character from "@/models/Character";
 import connect from "@/utils/db";
-import { NextResponse } from "next/server";
-
+import {NextResponse} from "next/server";
 
 //-----------------POST---------------------//
 
-export const POST = async (request) => {  
-    console.log(request.body);  
-    
-    const { name,
-            nation,
-            level,
-            health,
-            attack,
-            defense,
-            characteristic,
-            createdBy,    
-        } = await request.json();
+export const POST = async (request) => {
+  console.log(request.body);
 
-        await connect();
+  const {
+    name,
+    nation,
+    level,
+    health,
+    attack,
+    defense,
+    stadium,
+    characteristic,
+    createdBy,
+  } = await request.json();
 
-    const newChar = new Character({
-            name,
-            nation,
-            level,
-            health,
-            attack,
-            defense,
-            characteristic,
-            createdBy,
-    });
-    console.log(newChar);
+  await connect();
 
-    try {
-        const res = await newChar.save();
+  const newChar = new Character({
+    name,
+    nation,
+    level,
+    health,
+    attack,
+    defense,
+    stadium,
+    characteristic,
+    createdBy,
+  });
+  console.log(newChar);
 
-        return new NextResponse(JSON.stringify(res), {status: 201});
-    } catch (error) {
-        return new NextResponse("Database Error", {status: 500});
-    }
-}
+  try {
+    const res = await newChar.save();
+
+    return new NextResponse(JSON.stringify(res), {status: 201});
+  } catch (error) {
+    return new NextResponse("Database Error", {status: 500});
+  }
+};
 
 //----------------GET----------------------//
 export const GET = async (request) => {
-    console.log("Hello from GET");
-    try {
-        await connect();
-        const characters = await Character.findAll();
-        return new NextResponse(JSON.stringify(characters), {status: 200});
-    } catch (error) {
-        return new NextResponse("Database Error", {status: 500});
-    }
-}
+  console.log("Hello from GET");
+  try {
+    await connect();
+    const characters = await Character.findAll();
+    return new NextResponse(JSON.stringify(characters), {status: 200});
+  } catch (error) {
+    return new NextResponse("Database Error", {status: 500});
+  }
+};
 
 //-----------UPDATE-------------------//
 

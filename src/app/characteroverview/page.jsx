e602b0a094f2ec792bfb2912ba1227672a2122df
Don-Strevo <steven.guenther@dci.education>
2023-07-05 13:39:52 +0200

    no merge pls

--- src/app/characteroverview/page.jsx
+++ src/app/characteroverview/page.jsx
@@ -4,50 +4,49 @@ import Listitem from "@/components/Listitem/listitem";
 // import Charactercard from "@/components/cards/charactercard/charactercard";
 import {useSession} from "next-auth/react";
 import {useRouter} from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import styles from "./page.module.css";
 
 //----------------------------------------------------------------------------------------//
 
 const CharakterÜbersicht = () => {
-
   const router = useRouter();
-  const session = useSession(); 
+  const session = useSession();
   const [chardata, setChardata] = useState([]);
   const [selectedItemIndex, setSelectedItemIndex] = useState(null);
 
-//----------------------------------------------------------------------------------------//
+  //----------------------------------------------------------------------------------------//
 
-const getCharData = async () => {
-  const res = await fetch("/api/auth/character", {
-    // cache: "no-store",
-  });
+  const getCharData = async () => {
+    const res = await fetch("/api/auth/character", {
+      // cache: "no-store",
+    });
 
-  if(!res.ok){
-    throw new Error("Failed Datafetching")
+    if (!res.ok) {
+      throw new Error("Failed Datafetching");
+    }
+
+    const data = await res.json();
+    setChardata(data);
+    console.log("chardata", chardata);
   };
 
-  const data = await res.json();        
-  setChardata(data);
- } 
-
- //----------------------------------------------------------------------------------------//
+  //----------------------------------------------------------------------------------------//
 
   const handleItemClick = (index) => {
     setSelectedItemIndex(index);
     console.log("selected Char:", selectedItemIndex);
   };
 
-//----------------------------------------------------------------------------------------//
+  //----------------------------------------------------------------------------------------//
 
   const playWithCharacter = async (id) => {
     //was mache ich mit der ID?
     router.push("/game");
-  }
-//----------------------------------------------------------------------------------------//
+  };
+  //----------------------------------------------------------------------------------------//
 
   const deleteCharacter = async (id) => {
-
     try {
       await fetch(`/api/auth/character/${id}`, {
         method: "DELETE",
@@ -56,24 +55,23 @@ const getCharData = async () => {
       console.log(error);
     }
     getCharData();
- };
+  };
 
-//----------------------------------------------------------------------------------------//
+  //----------------------------------------------------------------------------------------//
 
   useEffect(() => {
-    if(session.data){  
-       getCharData();
+    if (session.data) {
+      getCharData();
     }
   }, [session]);
 
-//----------------------------------------------------------------------------------------//
+  //----------------------------------------------------------------------------------------//
 
   console.log(chardata);
 
   if (session.status === "unauthenticated") {
     router.push("/");
-
-   } else {
+  } else {
     return (
       <div className={styles.body}>
         {/* <h1 className={styles.characteroverviewtitle}></h1> */}
@@ -82,35 +80,46 @@ const getCharData = async () => {
             <p>Deine Charaktere:</p>
             <div>
               {chardata.map((char, index) => {
-                return <Listitem 
-                  key={index}
-                  index={index}
-                  name={char.name}
-                  nation={char.nation}
-                  level={char.level}
-                  onItemClick={handleItemClick}
-                  isSelected={selectedItemIndex === index}
+                return (
+                  <Listitem
+                    key={index}
+                    index={index}
+                    name={char.name}
+                    nation={char.nation}
+                    level={char.level}
+                    onItemClick={handleItemClick}
+                    isSelected={selectedItemIndex === index}
                   />
+                );
               })}
-            </div>          
+            </div>
           </div>
           <div className={styles.buttoncontainer}>
-            <button className={styles.playbutton} onClick={() => playWithCharacter(chardata[selectedItemIndex]._id)}>spielen</button>
-            <button className={styles.deletebutton} onClick={() => deleteCharacter(chardata[selectedItemIndex]._id)}>löschen</button>
-           </div>
+            <button
+              className={styles.playbutton}
+              onClick={() => playWithCharacter(chardata[selectedItemIndex]._id)}
+            >
+              spielen
+            </button>
+            <button
+              className={styles.deletebutton}
+              onClick={() => deleteCharacter(chardata[selectedItemIndex]._id)}
+            >
+              löschen
+            </button>
+          </div>
           <div
             className={styles.card}
             onClick={() => router.push("/charactercreation")}
           >
             <p>Neuen Charakter erstellen</p>
             <p>+</p>
-          </div> 
+          </div>
         </div>
-
       </div>
     );
-  }}
-
+  }
+};
 
 export default CharakterÜbersicht;
 
--- src/app/game/page.jsx
+++ src/app/game/page.jsx
@@ -1,12 +1,15 @@
+"use client";
 import React from "react";
 import styles from "./page.module.css";
+import NameOverview from "@/components/overview/name/nameoverview";
 
 function Game() {
   return (
     <div className={styles.body}>
       <div className={styles.container}>
+        <NameOverview />
         Auf dieser Seite wird gespielt!
-        </div>
+      </div>
     </div>
   );
 }

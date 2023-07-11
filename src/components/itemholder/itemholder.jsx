a592728b2b98a312ae339f059ea5f0ec48564f65
Madeline <madeline.streubel@gmx.de>
2023-07-11 13:22:57 +0200

    Inventaricons werden geladen, HoverFeld ist eingebaut.

--- src/components/itemholder/itemholder.jsx
+++ src/components/itemholder/itemholder.jsx
@@ -1,12 +1,25 @@
 import styles from "./itemholder.module.css";
 import React from "react";
+import Image from "next/image";
+
+//Bildimporte
+//import Testpic from "../../assets/items/testitem.png";
 
 function ItemHolder(props) {
 
   const {name, pic, price, text, type, id} = props;
-  console.log(name);
 
-  return <div className={styles.items}><img src="/testitem.png" alt="" />{name}</div>;
+  return <div className={styles.items}>
+    <Image 
+      src={pic} 
+      alt="Item" 
+      width={61} 
+      height={61}
+      />
+      <div className={styles.content}>
+        <p>{text}</p>
+      </div>
+    </div>;
 }
 
 export default ItemHolder;
